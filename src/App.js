17754c0aa60104efa1eab8d45657f3073b2c40f5
Riku Rouvila <riku.rouvila@gmail.com>
2016-10-25 14:41:28 +0300

    implement state handling inside component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const App = React.createClass({
+  getInitialState() {
+    return {
+      clicks: 0
+    };
+  },
+  onClick() {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  },
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
-        <p>Button clicked __ times</p>
-        <button>Click here</button>
+        <h1>Click counter</h1>
+        <p>Button clicked {this.state.clicks} times</p>
+        <button onClick={this.onClick}>Click here</button>
       </div>
     )
   }

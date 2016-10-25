d431b6b009b904fdc8e4249ee620779bb425c880
Riku Rouvila <riku.rouvila@gmail.com>
2016-10-25 14:30:42 +0300

    add clicking button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const App = React.createClass({
   render() {
-    return <h1>Hello world</h1>
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <p>Button clicked __ times</p>
+        <button>Click here</button>
+      </div>
+    )
   }
 });
 
